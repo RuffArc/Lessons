ddb6b1dfa17898578c38ea0c5d98bfbd34733327
Courtney Pope <courtney_pope@hotmail.com>
2013-10-18 17:13:00 -0400

    First 20 File Moved

---
+++ L11220/CheckSystemTime.java
@@ -0,0 +1,26 @@
+package Lessons.L11220;
+import java.text.DateFormat;
+import java.util.*;
+
+public class CheckSystemTime implements Runnable{
+	
+        @Override
+	public void run(){
+		
+		Date rightNow;
+		Locale currentLocale;
+		DateFormat timeFormatter;
+		String timeOutput;
+		
+		rightNow = new Date();
+		currentLocale = new Locale("en");
+		
+		timeFormatter = DateFormat.getTimeInstance(DateFormat.DEFAULT, currentLocale);
+		timeOutput = timeFormatter.format(rightNow);
+		
+		System.out.println("Time: " + timeOutput);
+		
+		
+	}
+	
+}
